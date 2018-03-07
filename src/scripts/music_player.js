3ed15dba3fd4b343c57a612f2bebbf6b329330e7
ciue <aoue.hao@gmail.com>
2018-03-07 20:34:51 +0800

    为audio添加错误处理

--- src/scripts/music_player.js
+++ src/scripts/music_player.js
@@ -1,4 +1,3 @@
-import { VKEY_URL } from './API.js'
 import {songUrl,lyricUrl,albumCoverUrl} from './playerURL.js'
 import {Progress} from './progress.js'
 import { Lyric } from './lyric.js'
@@ -6,6 +5,7 @@ import { Lyric } from './lyric.js'
 export class Music_player {
     constructor(el) {
         this.el = el
+        this.error = document.querySelector('#error')
         this.el.addEventListener('click', (e) => this.event(e))
         this.audio = this.createAudio()
         this.progress = new Progress(this.el.querySelector('#progress'))
@@ -14,41 +14,44 @@ export class Music_player {
     }
 
     async play(opts = {}) {
-        console.log(opts);
         let guid = Math.round(2147483647 * Math.random()) * (new Date).getUTCMilliseconds() % 1e10
-        let vkey = await this.getVkey(opts.songid,guid)
-        let song =  songUrl(opts.songid, vkey, guid)
-
+        let vkey = await this.getVkey(opts.songid, guid)
+        let song = songUrl(opts.songid, vkey, guid)
+        this.audio.src = song;
+        this.show();
         let ablumCover = albumCoverUrl(opts.albummid)
         this.el.querySelector('.song-name').innerHTML = decodeURI(opts.songname)
         this.el.querySelector('.singer-name').innerHTML = decodeURI(opts.singer)
         this.el.querySelector('.album-cover').src = ablumCover
         this.el.querySelector('.bg-blur').style.backgroundImage = `url(${ablumCover})`
         if (!this.fetching) {
-            this.audio.src = song;
-            fetch(lyricUrl(opts.lyricid))
-                .then(res => res.json())
-                .then(json => json.lyric)
-                .then(text => this.lyric.init(text))
-                .catch((e) => {
-                    console.log(e);
+            this.fetching = true
+            this.getLyric(opts.lyricid)
+                .then(() => {
+                    this.audio.play() //启动时播放
+                    this.progress.init(opts.duration) // 传入歌曲总时长
+                    this.fetching = false
                 })
-                .then(() => this.fetching = false)
-            this.audio.play() //启动时播放
-            this.progress.init(opts.duration) // 传入歌曲总时长
-            this.audio.volume = .3 // 调小声音
-            this.show()
+
         }
     }
 
     createAudio() {
-        let audio = document.createElement('audio')
+        let audio = document.createElement('audio') 
+        audio.addEventListener('error', () => {
+            console.log('资源出错了');
+            this.error.classList.add('show')
+            setTimeout(() => this.error.classList.remove('show'),1200)
+            window.location.hash = '#'
+        })
+
         audio.addEventListener('ended', () => {
             this.audio.play();
             this.progress.init();
             this.lyric.reset();
         })
         document.body.appendChild(audio)
+
         return audio
     }
 
@@ -109,6 +112,16 @@ export class Music_player {
             }
         })
 
+    }
+
+    getLyric(id) {
+        return fetch(lyricUrl(id))
+            .then(res => res.json())
+            .then(json => json.lyric)
+            .then(text => this.lyric.init(text))
+            .catch((e) => {
+                console.log(e);
+            })
     }
 
 }
